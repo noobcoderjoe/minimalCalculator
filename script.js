5511eaf09c7a84798632c8966a9c0d407f70e668
Jeison Joe <113160664+noobcoderjoe@users.noreply.github.com>
2022-10-16 14:46:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let screen = document.getElementById("screen");
+
+function display(num) {
+    screen.value += num;
+}
+
+function calculate() {
+
+    try {
+        screen.value = eval(screen.value);
+    }
+    catch (err) {
+        alert("Invalid input!");
+    }
+}
+
+function Clear() {
+    screen.value = " ";
+}
+
+function del() {
+    screen.value = screen.value.slice(0, -1);
+}
